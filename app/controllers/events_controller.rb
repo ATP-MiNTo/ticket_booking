dd44da46b2f6fd6920873475e1ed15e731f253a9
minto <markatpari@gmail.com>
2026-04-23 14:57:04 +0700

    Implement events index action and tickets availability calculation

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,15 @@
 class EventsController < ApplicationController
+    def index
+        events = Event.all
+
+        render json: events.map { |event|
+        {
+            id: event.id,
+            name: event.name,
+            date: event.date,
+            capacity: event.capacity,
+            tickets_available: event.tickets_available
+        }
+        }
+    end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,7 @@
 class Event < ApplicationRecord
     has_many :bookings
+
+    def tickets_available
+        capacity - bookings.sum(:quantity)
+    end
 end
